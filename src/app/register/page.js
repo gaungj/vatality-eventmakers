c7671d385ccc5841b1694cb97c71648f260e4b27
Wildan_A <wildanaminudin16@gmail.com>
2024-07-01 06:32:41 +0700

    menambah hash password

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -10,69 +10,30 @@ export default function page() {
           </h2>
           <form action={registerAction} className="space-y-6">
             <div>
-              <label
-                for="username"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Username
-              </label>
+              <label htmlFor="username">Username</label>
               <div className="mt-1">
-                <input
-                  name="username"
-                  type="username"
-                  required
-                  className="px-2 py-3 mt-1 block w-full rounded-md border border-gray-300 shadow-sm focus:border-sky-500 focus:outline-none focus:ring-sky-500 sm:text-sm"
-                />
+                <input name="username" type="username" required />
               </div>
             </div>
             <div>
-              <label
-                for="email"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Email
-              </label>
+              <label htmlFor="email">Email</label>
               <div className="mt-1">
                 <input
                   name="email"
                   type="email-address"
-                  autocomplete="email-address"
+                  autoComplete="email-address"
                   required
-                  className="px-2 py-3 mt-1 block w-full rounded-md border border-gray-300 shadow-sm focus:border-sky-500 focus:outline-none focus:ring-sky-500 sm:text-sm"
                 />
               </div>
             </div>
             <div>
-              <label
-                for="password"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Password
-              </label>
+              <label htmlFor="password">Password</label>
               <div className="mt-1">
                 <input
                   name="password"
                   type="password"
-                  autocomplete="password"
+                  autoComplete="password"
                   required
-                  className="px-2 py-3 mt-1 block w-full rounded-md border border-gray-300 shadow-sm focus:border-sky-500 focus:outline-none focus:ring-sky-500 sm:text-sm"
-                />
-              </div>
-            </div>
-            <div>
-              <label
-                for="password"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Confirm Password
-              </label>
-              <div className="mt-1">
-                <input
-                  name="confirm_password"
-                  type="password"
-                  autocomplete="confirm-password"
-                  required
-                  className="px-2 py-3 mt-1 block w-full rounded-md border border-gray-300 shadow-sm focus:border-sky-500 focus:outline-none focus:ring-sky-500 sm:text-sm"
                 />
               </div>
             </div>
