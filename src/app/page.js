dc91009a8bb3433646c87c1b3db07b18f6099f54
gaungj <gaung.jgd@gmail.com>
2024-07-01 16:11:14 +0700

    feat: landing page

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,26 @@
-export default function Home() {
-  return <main>Home</main>;
+import { Header } from "@/components/header";
+import Link from "next/link";
+import { Card } from "@/components/card";
+import { prisma } from "@/utils/prisma";
+import React from "react";
+
+export default async function Home() {
+  const events = await prisma.event.findMany();
+
+  return (
+    <main>
+      <Header />
+      <div className="container flex flex-col items-center m-auto py-12">
+        <section className="mb-20">
+          <h1>Create and Discover Nearby Events</h1>
+          <p className="text-center">Make Connections within Community</p>
+        </section>
+        <section className="grid grid-cols-3 gap-6">
+          {events.map((event) => (
+            <Card key={event.id} event={event} />
+          ))}
+        </section>
+      </div>
+    </main>
+  );
 }
